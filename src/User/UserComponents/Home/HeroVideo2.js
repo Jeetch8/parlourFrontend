ccf0860ed87d9e060187b8cdb8f99311ab113990
JeetCh9 <jeetkumar0898@gmail.com>
2022-11-23 18:42:59 +0530

    Video changes

--- src/User/UserComponents/Home/HeroVideo2.js
+++ src/User/UserComponents/Home/HeroVideo2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { Box } from "@chakra-ui/react";
+import { Box, position } from "@chakra-ui/react";
 
 const HeroVideo2 = () => {
   return (
@@ -31,6 +31,14 @@ const HeroVideo2 = () => {
         src="https://streamable.com/e/sjj8y2?autoplay=1&nocontrols=1"
         width={"100%"}
         height="100%"
+        allow="autoplay"
+        style={{
+          position: "absolute",
+          border: "0",
+          left: "0",
+          top: "0",
+          overflow: "hidden",
+        }}
       ></embed>
     </Box>
   );
